38fa61799477b40d3a42665205e4af93a413cac1
Tiago Lima <tiagolima@osf.digital>
2026-04-07 13:49:43 -0300

    feat: protected routes by user roles.

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,52 @@
+import { type NextFunction, type Request, type Response } from "express";
+import User, { type IUser, type userRoles } from "../models/user";
+import jwt from "jsonwebtoken";
+
+export interface IAuthRequest extends Request {
+  user?: IUser;
+}
+
+// Protect routes middleware
+export const protect = async (req: IAuthRequest, res: Response, next: NextFunction) => {
+  let token;
+
+  // check for token in cookies //not token but jwt
+  if (req.cookies && req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (token) {
+    try {
+      const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
+      req.user = (await User.findById(decoded.userId).select("-password")) as IUser;
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
+
+/**
+ * Accepts a list of allowed roles (e.g. 'admin', 'teacher')
+ * usage: router.post('/', protect, authorize('admin'), createClass)
+ */
+
+export const authorize = (roles: userRoles[]) => {
+  return (req: IAuthRequest, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      return res.status(401).json({ message: "Not authorized, user not found" });
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: `User role '${req.user.role}' is not authorized to access this route`,
+      });
+    }
+
+    // user has permission to proceed
+    next();
+  };
+};
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,8 +3,9 @@ import express from "express";
 const userRoutes = express.Router();
 
 import { login, registerUser } from "../controllers/user";
+import { authorize, protect } from "../middlewares/auth";
 
-userRoutes.post("/register", registerUser);
+userRoutes.post("/register", protect, authorize(["admin", "teacher"]), registerUser);
 userRoutes.post("/login", login);
 
 export default userRoutes;
