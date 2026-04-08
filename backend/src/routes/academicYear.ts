21a1b27d463dea38e975a7b8ed2008fd59391e3f
Tiago Lima <tiagolima@osf.digital>
2026-04-08 14:27:41 -0300

    feat: added the academic year endpoints.

--- backend/src/routes/academicYear.ts
+++ backend/src/routes/academicYear.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import { authorize, protect } from "../middlewares/auth";
-import { createAcademicYear } from "../controllers/academicYear";
+import {
+  createAcademicYear,
+  deleteAcademicYear,
+  getAllAcademicYears,
+  getCurrentAcademicYear,
+  updateAcademicYear,
+} from "../controllers/academicYear";
 
 const academicYearRoutes = express.Router();
 
+academicYearRoutes.route("/").get(protect, authorize(["admin"]), getAllAcademicYears);
+
 academicYearRoutes.route("/create").post(protect, authorize(["admin"]), createAcademicYear);
+academicYearRoutes.route("/current").post(protect, authorize(["admin"]), getCurrentAcademicYear);
+academicYearRoutes.route("/update/:id").patch(protect, authorize(["admin"]), updateAcademicYear);
+academicYearRoutes.route("/delete/:id").delete(protect, authorize(["admin"]), deleteAcademicYear);
 
 export default academicYearRoutes;
